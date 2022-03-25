bd46a3ebff510a747e9a7054e62b3babf78a1401
BeansAndPeas <72360102+BeansAndPeas@users.noreply.github.com>
2022-03-25 12:16:40 +0000

    Resource bar and start minimap

---
+++ Assets/Resources/Code/UIController.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Resources.Code {
+    public class UIController : MonoBehaviour {
+        [SerializeField] 
+        private RawImage minimap;
+        [SerializeField]
+        private Camera minimapCamera;
+
+        
+    }
+}
